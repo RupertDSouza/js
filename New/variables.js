2064f9069b3f53043f36c3eb7e968e78001290d8
Anish Rupert D Souza <61092071+RupertDSouza@users.noreply.github.com>
2023-10-20 13:57:41 +0000

    new file 1

---
+++ New/variables.js
@@ -0,0 +1,17 @@
+const acc_id = 12233;
+let acc_email = "anish@gmail.com"
+var acc_password = "12345" 
+accCity = "Mangalore";
+acc_state;
+/*
+try not to use var
+*/
+
+//acc_id = 133
+acc_email = "rupert@gmail.com"
+acc_password = "6789"
+accCity = "Mumbai"
+
+console.log(acc_id);
+
+console.table([acc_email, acc_password, accCity, acc_state])
