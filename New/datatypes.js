adf13d7fc9c3b711ddf355d166046e8916a8695c
Anish Rupert D Souza <61092071+RupertDSouza@users.noreply.github.com>
2023-10-20 14:29:57 +0000

    datatype

---
+++ New/datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all JS code as never version
+
+//alert("hello") // works on browser and not in nodejs engine
+
+console.log( 3 + 3); console.log("hello") //not a good practice
+
+/*
+    not a good way of writing code
+    code readability is important
+*/
+
+let name = "Rupert" // String
+let age = 18 //number
+let isLoggedIn = false //boolean
+
+// number => 2 to pow(53)
+// bigint
+// string => ""
+// boolean => true/false
+// null => assigned for empty value //standalone value
+// undefined //not s single value assigned, not even null is assigned
+// symbol => unique
+
+// object
+
+console.log(typeof name);
+console.log(typeof undefined); //undefined
+console.log(typeof null) //object
