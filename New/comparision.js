1dc13a00b3c4e9c882f32980eab34aaffded50cb
Anish Rupert D Souza <61092071+RupertDSouza@users.noreply.github.com>
2023-10-21 11:56:05 +0000

    studynote05

---
+++ New/comparision.js
@@ -0,0 +1,25 @@
+console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 < 1);
+console.log(2 == 1);
+console.log(2 != 1);
+
+console.log("2" > 1);
+console.log(2 > "1");
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+console.log(0 > null);
+console.log(undefined > 0)
+console.log(undefined < null)
+
+
+console.log("Anish" < "hai")
+console.log("Anish" > "hai")
+console.log("haiiiii" > "Anish")
+console.log("Anish" > "haiiiii")
+console.log("Anish" == "hai")
+
+console.log("2"===2); // use this type of comparision than "==" this 
+console.log("2"==="2");
